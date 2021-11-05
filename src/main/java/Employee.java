b57556ce44af0016f0f30a9a7b8a4350cd5a1395
rpo <hakimnaufal212@gmail.com>
2021-11-05 22:18:58 +0700

    initial commit

---
+++ src/main/java/Employee.java
@@ -0,0 +1,26 @@
+public class Employee {
+
+//      Nama Class diawali dengan huruf besar + camelCase
+//      Nama variable diawali dengan huruf kecil + camelCase
+//      Nama method diawali dengan huruf kecil + camelCase
+//      Nama package diawali dengan huruf kecil + camelCase
+
+    // How to declare variable
+    // 1. declare variable type of the variable
+    // 2. declare variable name
+    // 3. declare variable value
+    String countryName = "Indonesia";
+    public static void main(String[] args){
+        String countryName1 = "Indonesia";
+        System.out.println(countryName1);
+        System.out.println("Hello World");
+
+    }
+    public int sum(int a, int b){
+        return a+b;
+    }
+    public void printCountryName(){
+        
+        System.out.println(countryName);
+    }
+}
